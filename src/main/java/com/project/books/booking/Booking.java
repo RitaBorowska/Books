91961f6cb2d235075cb02e423f0468cfb608ee75
RitaBorowska <ritaboczulak@gmail.com>
2021-01-10 13:22:07 +0100

    create entity

---
+++ src/main/java/com/project/books/booking/Booking.java
@@ -0,0 +1,27 @@
+package com.project.books.booking;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+    private LocalDate dateToPicUp;
+    private String typeOfPickUp;
+
+}
---
+++ src/main/java/com/project/books/user/User.java
@@ -0,0 +1,31 @@
+package com.project.books.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long userId;
+    private String name;
+    private String surname;
+    private String login;
+    private String password;
+    private String address;
+    private String dateOfRegistration;
+}
